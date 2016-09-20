9d1ff14de49693aba857440115dca9708793d1fe
PierreGoude <linoge80@live.se>
2016-09-20 10:25:35 +0200

    Update ADconnection.sh

--- ADconnection.sh
+++ ADconnection.sh
@@ -52,7 +52,7 @@ sudo echo "$NetBios"'\'"domain^admins" >> /etc/ssh/login.group.allowed
 sudo echo "$NetBios"'\'"$myhost""sudoers" >> /etc/ssh/login.group.allowed
 sudo echo "%$NetBios"'\\'"domain^admins ALL=(ALL:ALL) ALL" >> /etc/sudoers
 sudo echo "%$NetBios"'\\'"$myhost""sudoers ALL=(ALL:ALL) ALL" >> /etc/sudoers
-sudo echo "$user"" ALL=(ALL:ALL) ALL" >> /etc/sudoers 
+sudo echo "$UseR"" ALL=(ALL:ALL) ALL" >> /etc/sudoers 
 sudo echo "%DOMAIN\ admins@$DOMAIN ALL=(ALL) ALL" >> /etc/sudoers.d/domain_admins
 
 while true; do
