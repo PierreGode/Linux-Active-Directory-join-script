5895801c876fd7345f1b2833046341803668836a
PierreGoude <linoge80@live.se>
2016-12-12 16:47:38 +0100

    Update ADconnection.sh

--- ADconnection.sh
+++ ADconnection.sh
@@ -57,6 +57,7 @@ exit' > tmp.sh
 xterm -e "sudo sh tmp.sh"
 sudo systemctl enable sssd
 sudo systemctl start sssd
+sudo rm tmp.sh
 echo "session required pam_mkhomedir.so skel=/etc/skel/ umask=0022" >> /etc/pam.d/common-session
 echo "auth required pam_listfile.so onerr=fail item=group sense=allow file=/etc/ssh/login.group.allowed" >> /etc/pam.d/common-auth
 sudo sh -c "echo 'greeter-show-manual-login=true' >> /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf"
