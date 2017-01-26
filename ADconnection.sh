579093548e80055db1c3883ebc7422906204c5f7
PierreGoude <pierre@webbhatt.com>
2017-01-26 11:38:44 +0100

    Update ADconnection.sh

--- ADconnection.sh
+++ ADconnection.sh
@@ -59,15 +59,15 @@ sudo touch /etc/ssh/login.group.allowed
 sudo echo "administrator" >> /etc/ssh/login.group.allowed
 sudo echo "$NetBios"'\'"$myhost""sudoers" >> /etc/ssh/login.group.allowed
 sudo echo "$NetBios"'\'"$UseR" >> /etc/ssh/login.group.allowed
-sudo echo "administrator ALL=(ALL:ALL) ALL" >> /etc/sudoers
+sudo echo "administrator ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/users
 sudo echo "$NetBios"'\'"domain^admins" >> /etc/ssh/login.group.allowed
 sudo echo "$NetBios"'\'"$myhost""sudoers" >> /etc/ssh/login.group.allowed
-sudo echo "%domain^admins ALL=(ALL:ALL) ALL" >> /etc/sudoers
-sudo echo "%$myhost""sudoers ALL=(ALL:ALL) ALL" >> /etc/sudoers
+sudo echo "%domain^admins ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/users
+sudo echo "%$myhost""sudoers ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/users
 sudo echo "%DOMAIN\ admins@$DOMAIN ALL=(ALL) ALL" >> /etc/sudoers.d/domain_admins
 echo "Check that the group is correct"
 echo "In Sudoers file..."
-sudo cat /etc/sudoers | grep $myhost
+sudo cat /etc/sudoers.d/users | grep $myhost
 echo "In SSH allow file..."
 sudo cat /etc/ssh/login.group.allowed | grep $myhost
 echo "If this is wrong DO NOT REBOOT and contact sysadmin"
@@ -113,15 +113,15 @@ sudo touch /etc/ssh/login.group.allowed
 sudo echo "administrator" >> /etc/ssh/login.group.allowed
 sudo echo "$NetBios"'\'"$myhost""sudoers" >> /etc/ssh/login.group.allowed
 sudo echo "$NetBios"'\'"$UseR" >> /etc/ssh/login.group.allowed
-sudo echo "administrator ALL=(ALL:ALL) ALL" >> /etc/sudoers
+sudo echo "administrator ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/users
 sudo echo "%domain^admins" >> /etc/ssh/login.group.allowed
 sudo echo "$NetBios"'\'"$myhost""sudoers" >> /etc/ssh/login.group.allowed
-sudo echo "$NetBios"'\\'"domain^admins ALL=(ALL:ALL) ALL" >> /etc/sudoers
-sudo echo "%$myhost""sudoers ALL=(ALL:ALL) ALL" >> /etc/sudoers
+sudo echo "%domain^admins ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/users
+sudo echo "%$myhost""sudoers ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/users
 sudo echo "%DOMAIN\ admins@$DOMAIN ALL=(ALL) ALL" >> /etc/sudoers.d/domain_admins
 echo "Check that the group is correct"
 echo "In Sudoers file..."
-sudo cat /etc/sudoers | grep $myhost
+sudo cat /etc/sudoers.d/users | grep $myhost
 echo "In SSH allow file..."
 sudo cat /etc/ssh/login.group.allowed | grep $myhost
 echo "If this is wrong DO NOT REBOOT and contact sysadmin"
