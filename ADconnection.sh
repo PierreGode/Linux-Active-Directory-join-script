0ad2544e9eac4c888599a4962b52e66d8c705309
PierreGoude <pierre@webbhatt.com>
2017-02-02 08:59:24 +0100

    Update ADconnection.sh

--- ADconnection.sh
+++ ADconnection.sh
@@ -93,12 +93,35 @@ sudo echo "$NetBios"'\'"$myhost""sudoers" >> /etc/ssh/login.group.allowed
 sudo echo "%domain^admins ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/sudoers
 sudo echo "%$myhost""sudoers ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/sudoers
 sudo echo "%DOMAIN\ admins@$DOMAIN ALL=(ALL) ALL" >> /etc/sudoers.d/domain_admins
-echo "Check that the group is correct"
-echo "In Sudoers file..."
-sudo cat /etc/sudoers.d/sudoers | grep sudoers
-echo "In SSH allow file..."
-sudo cat /etc/ssh/login.group.allowed | grep $myhost
-echo "If this is wrong DO NOT REBOOT and contact sysadmin"
+therealm=$(realm discover | grep -i configured: | cut -d ':' -f2 | sed -e 's/^[[:space:]]*//')
+guest=$(cat /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf | grep -i allow-guest | grep -i false | cut -d '=' -f2)
+if [ $therealm = no ]
+then
+echo Realm configured?.. "${RED_TEXT}"FAIL"${END}"
+else
+echo Realm configured?.. "${INTRO_TEXT}"OK"${END}"
+fi
+
+if [ -f /etc/sudoers.d/sudoers ]
+then
+echo Checking sudoers file..  "${INTRO_TEXT}"OK"${END}"
+grouPs=$(cat /etc/sudoers.d/sudoers | grep -i $myhost | cut -d '%' -f2 | cut -d  '=' -f1 | sed -e 's/\<ALL\>//g')
+if [ $grouPs = "$myhost""sudoers" ]
+then 
+echo Checking sudoers users.. "${INTRO_TEXT}"OK"${END}"
+else
+echo Checking sudoers users.. "${RED_TEXT}"FAIL"${END}"
+fi
+else
+echo checking sudoers file..  "${RED_TEXT}"FAIL"${END}"
+echo Checking sudoers users.. "${RED_TEXT}"FAIL"${END}"
+fi
+if [ $guest = false ]
+then
+echo Checking login configuration.. "${INTRO_TEXT}"OK"${END}"
+else
+echo Checking login configuration.. "${RED_TEXT}"FAIL"${END}"
+fi
 exec sudo -u root /bin/sh - <<eof
 sed -i -e 's/fallback_homedir = \/home\/%u@%d/#fallback_homedir = \/home\/%u@%d/g' /etc/sssd/sssd.conf
 sed -i -e 's/use_fully_qualified_names = True/use_fully_qualified_names = False/g' /etc/sssd/sssd.conf
