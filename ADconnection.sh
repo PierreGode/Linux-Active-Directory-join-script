17816a918872f3bfabb9863bb6f6611f9f1942fa
PierreGoude <pierre@webbhatt.com>
2017-06-14 14:15:19 +0200

    Create ADconnection.sh

--- ADconnection.sh
+++ ADconnection.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#####################################################################################################################
-#                                                                                                                   #
-#                              This script is written by Pierre Goude                                               #
-#  This program is open source; you can redistribute it and/or modify it under the terms of the GNU General Public  #
-#                     This is an normal bash script and can be executed with sh                                     #
-# Generic user setup is: administrator, domain admins, groupnamesudores= groupname=hostname + sudoers on groupname  #
-#####################################################################################################################
+##################################################################################################################################
+#                                                                                                                                #
+#                              This script is written by Pierre Goude                                                            #
+#  This program is open source; you can redistribute it and/or modify it under the terms of the GNU General Public               #
+#                     This is an normal bash script and can be executed with sh                                                  #
+# Generic user setup is: administrator, domain admins, groupnamesudores= groupname=hostname + sudoers on groupname in AD groups  #
+##################################################################################################################################
 
 #known bugs: see line 23-24
 
