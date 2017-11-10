693a90f0cc60e8a2cc7f02b0a05ada171e0d72e3
PierreGoude <pierre@webbhatt.com>
2017-11-10 11:02:52 +0100

    Active directory Join script for Ubuntu, Debian, CentOS and Raspberry Pi with built in failchcheck and debugmode for Ubuntu. "The most advanced AD join script on GITHUB"
    I the latest update i removed sss from sudoers file in /etc/nsswitch.conf This has the effect of avoiding re-authentication against the AD for every new interactive shell. Instead, cached credentials from the first login will be used.

--- ADconnection.sh
+++ ADconnection.sh
@@ -229,6 +229,7 @@ esac
 sed -i -e 's/fallback_homedir = \/home\/%u@%d/#fallback_homedir = \/home\/%u@%d/g' /etc/sssd/sssd.conf
 sed -i -e 's/use_fully_qualified_names = True/use_fully_qualified_names = False/g' /etc/sssd/sssd.conf
 sed -i -e 's/access_provider = ad/access_provider = simple/g' /etc/sssd/sssd.conf
+sed -i -e 's/sudoers:        files sss/sudoers:        files/g' /etc/nsswitch.conf
 echo "override_homedir = /home/%d/%u" | sudo tee -a /etc/sssd/sssd.conf
 cat /etc/sssd/sssd.conf | grep -i override
 sudo service sssd restart
