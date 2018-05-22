597c0a62461ec74768b00f24971cd206f17ae7f4
PierreGode <pierre@webbhatt.com>
2018-05-22 08:54:40 +0200

    Update ADconnection.sh

--- ADconnection.sh
+++ ADconnection.sh
@@ -5,7 +5,7 @@
 #      This program is open source; you can redistribute it and/or modify it under the terms of the GNU General Public           #
 #                     This is an normal bash script and can be executed with sh EX: ( sudo sh ADconnection.sh )                  #
 # Generic user setup is: administrator, domain admins, groupnamesudores= groupname=hostname + sudoers on groupname in AD groups  #
-#                                                                                                                                #
+#           note: the script will NOT manipulate any files until the join to active directory is successful                      #
 ##################################################################################################################################
 #known bugs: Sometimes the script bugs after AD administrator tries to authenticate, temporary solution is running the script again
 # a couple of times. if it still is not working see line 24-25
